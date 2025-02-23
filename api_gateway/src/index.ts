5535b42753e5726de7d1ff4b6dab0e072fc4ae51
RydKrm <alriyadkarim@gmail.com>
2025-02-23 08:04:36 +0600

    API-Gateway : Create chain and handler

---
+++ api_gateway/src/index.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import morgan from "morgan";
+import { configureRoutes } from "./utils";
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  handler: (_req: Request, res: Response) => {
+    res.status(429).json({
+      message: "Too many requests, please try again later.",
+    });
+  },
+});
+
+app.use("/api", limiter);
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+configureRoutes(app);
+
+app.get("/health", (_req: Request, res: Response) => {
+  res.status(200).json({
+    message: "API Gateway is running",
+  });
+});
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`API Gateway is running on port ${PORT}`);
+});
---
+++ api_gateway/src/utils.ts
@@ -0,0 +1,53 @@
+import { Express, Request, Response } from "express";
+import axios from "axios";
+import config from "./config.json";
+
+type HttpMethod = "get" | "post" | "put" | "delete" | "patch";
+
+const createHandler = (hostname: string, path: string, method: string) => {
+  return async (req: Request, res: Response) => {
+    try {
+      let url = `${hostname}/${path}`;
+      if (req.params) {
+        Object.keys(req.params).forEach((param: string) => {
+          url = url.replace(`:${param}`, req.params[param]);
+        });
+      }
+
+      const { data } = await axios({
+        url,
+        method,
+        data: req.body,
+      });
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+
+      if (err instanceof axios.AxiosError) {
+        return res.status(err.response?.status || 500).json({
+          message: err.response?.data || "Server calling error",
+        });
+      }
+
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  };
+};
+
+export const configureRoutes = (app: Express) => {
+  Object.entries(config.services).forEach(([name, service]) => {
+    const hostname = service.url;
+    service.routes.forEach((route) => {
+      route.method.forEach((method) => {
+        const handler = createHandler(hostname, route.path, method);
+        console.log(`${method} - ${hostname}/${route.path} `);
+        app[method.toLowerCase() as HttpMethod](
+          `/api/v1/${route.path}`,
+          handler
+        );
+      });
+    });
+  });
+};
