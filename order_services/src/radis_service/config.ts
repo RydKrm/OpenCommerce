d8e3b279a346ea48d00f3e71bf13a6cf7b057385
RydKrm <alriyadkarim@gmail.com>
2025-03-07 11:47:32 +0600

    Add message broker & Radis

---
+++ order_services/src/radis_service/config.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: ".env",
+});
+
+export const REDIS_PORT = process.env.REDIS_PORT
+  ? parseInt(process.env.REDIS_PORT)
+  : 6379;
+export const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+export const CART_TTL = process.env.CART_TTL
+  ? parseInt(process.env.CART_TTL)
+  : 9000;
