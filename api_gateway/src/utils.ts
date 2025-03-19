ea488bc90e955f596ed5670dcaf1e165c7f4956a
Al Riyad Karim <riyad.inno@pakiza.com>
2025-03-19 11:36:57 +0600

    feat: Implement API Gateway (#3)

--- api_gateway/src/utils.ts
+++ api_gateway/src/utils.ts
@@ -1,59 +1,69 @@
 import { Express, Request, Response } from "express";
 import axios from "axios";
-import config from "./config.json";
+import app_routes from "./routes";
 import middlewares from "./middlewares";
+import createHandler from "./createHandler";
 
 type HttpMethod = "get" | "post" | "put" | "delete" | "patch";
 
-const createHandler = (hostname: string, path: string, method: string) => {
-  return async (req: Request, res: Response) => {
-    try {
-      let url = `${hostname}/${path}`;
-      if (req.params) {
-        Object.keys(req.params).forEach((param: string) => {
-          url = url.replace(`:${param}`, req.params[param]);
-        });
-      }
+// const createHandler = (hostname: string, path: string, method: string) => {
+//   return async (req: Request, res: Response) => {
+//     try {
+//       let url = `${hostname}/${path}`;
+//       if (req.params) {
+//         Object.keys(req.params).forEach((param: string) => {
+//           url = url.replace(`:${param}`, req.params[param]);
+//         });
+//       }
 
-      const { data } = await axios({
-        url,
-        method,
-        data: req.body,
-      });
-      res.json(data);
-    } catch (err) {
-      console.log(err);
+//       console.log(url);
 
-      if (err instanceof axios.AxiosError) {
-        return res.status(err.response?.status || 500).json({
-          message: err.response?.data || "Server calling error",
-        });
-      }
+//       const { data } = await axios({
+//         url,
+//         method,
+//         data: req.body,
+//       });
+//       res.json(data);
+//     } catch (err) {
+//       console.log(err);
 
-      res.status(500).json({
-        message: "Internal Server Error",
-      });
-    }
-  };
-};
+//       if (err instanceof axios.AxiosError) {
+//         return res.status(err.response?.status || 500).json({
+//           message: err.response?.data || "Server calling error",
+//         });
+//       }
 
-export const getMiddleware = (names: string[]) => {
-  // @ts-ignore
-  return names.map((name) => middlewares[name]);
+//       res.status(500).json({
+//         message: "Internal Server Error",
+//       });
+//     }
+//   };
+// };
+
+export const getMiddleware = (names: string[], roles: string[] = []) => {
+  const middleware_list: any = [];
+  if (names.find((name) => name.toLocaleLowerCase() === "auth")) {
+    middleware_list.push(middlewares.auth(roles));
+  }
+  return middleware_list;
 };
 
 export const configureRoutes = (app: Express) => {
-  Object.entries(config.services).forEach(([name, service]) => {
+  Object.entries(app_routes).forEach(([name, service]) => {
     const hostname = service.url;
     service.routes.forEach((route) => {
       route.method.forEach((method) => {
         const handler = createHandler(hostname, route.path, method);
+        const role = route?.role ? route?.role : [];
+        const middleware = getMiddleware(route.middlewares, role);
 
-        const middleware = getMiddleware(route.middlewares);
+        console.log(
+          `${method} - ${role ? role : "public"} - ${hostname}/${route.path} `
+        );
 
-        console.log(`${method} - ${hostname}/${route.path} `);
         app[method.toLowerCase() as HttpMethod](
-          `/api/v1/${route.path}`,
+          `/api/${route.path}`,
+          // middlewares.auth(role),
           middleware,
           handler
         );
